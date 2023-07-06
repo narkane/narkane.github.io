aa22081b44dec8669a4dd93c23a6f4b0278eb881
narkane <enikath@gmail.com>
2023-07-06 05:02:10 -0500

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = (index) => {
+  // console.log(index, title, icon);
+  console.log(index.title);
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img
+            src={index.icon}
+            alt={index.title}
+            className="w-16 h-16 object-contain"
+          />
+          <h3 className="test-white text-[20px] font-bold text-center">
+            {index.title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        // test
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Im super great!
+        <br />
+        [-DESCRIPTION HERE-]
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard
+            key={service.title}
+            index={index}
+            icon={service.icon}
+            title={service.title}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
